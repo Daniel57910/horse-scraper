999f5ee86c1ad74caf7f565e868064b1e129aea1
Daniel Miller <danielmiller5791@gmail.com>
2018-06-26 15:21:22 +0100

    updated ignore file

--- models/siteSchema.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var siteSchema = mongoose.Schema({
-  siteData: { type: String }
-});
-
-var siteData = mongoose.model("sample_site", siteSchema);
-module.exports = siteData;
