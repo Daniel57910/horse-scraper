8a2980976ec299e95be580ac479d7aea901f8506
Daniel Miller <danielmiller5791@gmail.com>
2018-07-10 11:29:55 +0100

    basic refactoring with classes completed

--- src/oddsSelector.js
+++ src/oddsSelector.js
@@ -1,24 +1,27 @@
-function oddsSelector() {
-  this.oddsData = [];
-  oddsArray = [];
-}
+class oddsSelector {
 
-oddsSelector.prototype.findOdds = function (data, htmlClass, htmlTag) {
-  console.log("ODDS FOR HORSES = ")
-  this.oddsData = data(htmlClass);
-  for (let i = 0; i < this.oddsData.length; i++) {
-    getTheOdds(this.oddsData[i]);
+  constructor() {
+    this.oddsData = [];
   }
-  return oddsArray;
+
+  findOdds (data, htmlClass, htmlTag) {
+    var oddsArray = [];
+    this.oddsData = data(htmlClass);
+    for (let i = 0; i < this.oddsData.length; i++) {
+      getTheOdds(this.oddsData[i], oddsArray);
+    }
+    return oddsArray;
+  }
+
 }
 
-
-function getTheOdds(element) {
+function getTheOdds(element, oddsArray) {
   currentData = [];
   for (let i = 0; i < 23; i++) {
     currentData.push(element.children[i].next.attribs["data-o"]);
   }
   oddsArray.push(currentData);
 }
+
 
 module.exports = oddsSelector;
