c7f26ecc901811382e0125a36e90d6db012afe56
Daniel Miller <danielmiller5791@gmail.com>
2018-07-10 11:14:12 +0100

    first class created, need to refactor for loops

--- src/oddsSelector.js
+++ src/oddsSelector.js
@@ -1,24 +1,29 @@
-function oddsSelector() {
-  this.oddsData = [];
-  oddsArray = [];
-}
+class oddsSelector  {
 
-oddsSelector.prototype.findOdds = function (data, htmlClass, htmlTag) {
-  console.log("ODDS FOR HORSES = ")
-  this.oddsData = data(htmlClass);
-  for (let i = 0; i < this.oddsData.length; i++) {
-    getTheOdds(this.oddsData[i]);
+  constructor() {
+    this.oddsData = [];
+    this.oddsArray = [];
   }
-  return oddsArray;
-}
 
 
-function getTheOdds(element) {
+  findOdds(data, htmlClass, htmlTag) {
+    console.log("ODDS FOR HORSES = ")
+    this.oddsData = data(htmlClass);
+    for (let i = 0; i < this.oddsData.length; i++) {
+      getTheOdds(this.oddsData[i]);
+    }
+    return oddsArray;
+  }
+
+
+getTheOdds(element) {
   currentData = [];
   for (let i = 0; i < 23; i++) {
     currentData.push(element.children[i].next.attribs["data-o"]);
   }
   oddsArray.push(currentData);
+}
+
 }
 
 module.exports = oddsSelector;
